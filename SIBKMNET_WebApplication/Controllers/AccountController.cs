139b438895b882f18ceb61b22fa6cb442194ea40
cahyooo <toriqzuhudnurcahyo@gmail.com>
2022-10-03 15:14:03 +0700

    initial commit

---
+++ SIBKMNET_WebApplication/Controllers/AccountController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SIBKMNET_WebApplication.Repositories.Data;
+using SIBKMNET_WebApplication.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SIBKMNET_WebApplication.Controllers
+{
+    public class AccountController : Controller
+    {
+        AccountRepository accountRepository;
+
+        public AccountController(AccountRepository accountRepository)
+        {
+            this.accountRepository = accountRepository;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Login(Login login)
+        {
+            // ambil data dari database sesuai dengan email dan pass
+            //if (ModelState.IsValid)
+            //{
+                var data = accountRepository.Login(login);
+                if (data != null)
+                {
+                    // inisialisasi nilai pada session
+                    HttpContext.Session.SetString("Role", data.Role);
+                    return RedirectToAction("Index", "Province");
+                }
+                return View();
+            //}
+            return View();
+        }
+
+
+    }
+}
